f9d84d79f6e76800682c234abd7c024f74271bb2
lxb7267247 <a7267247@163.com>
2021-07-29 17:39:02 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -16,6 +16,7 @@ $.ajaxPrefilter(function(options) {
     // 全局统一挂载 complete 回调函数
     // 不论成功还是失败，最终都会调用 complete 回调函数
     options.complete = function(res) {
+        // console.log(res);
         // 在 complete中 可以使用 res.responseJSON拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             // 强制清空 token
